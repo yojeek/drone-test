752e54721110f29199f9655e8c4f432639d3e018
torinasakura <me@torinasakura.name>
2018-01-29 16:54:26 +0300

    #Containers
    - refactor Root
    - TODO in routes
    
    #Environment
    - change nameSpace's

--- src/app/containers/Root.js
+++ src/app/containers/Root.js
@@ -3,24 +3,16 @@ import { PersistGate } from 'redux-persist/es/integration/react'
 import { ConnectedRouter } from 'react-router-redux'
 import { Provider } from 'react-redux'
 import { hot } from 'react-hot-loader'
-import Preload from './Preload'
 import Routes from './Routes'
 
 const Root = ({ store, history, persistor }) => (
-  <AppContainer>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Preload
-          history={history}
-          store={store}
-        >
-          <ConnectedRouter history={history}>
-            <Routes store={store} />
-          </ConnectedRouter>
-        </Preload>
-      </PersistGate>
-    </Provider>
-  </AppContainer>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <Routes store={store} />
+      </ConnectedRouter>
+    </PersistGate>
+  </Provider>
 )
 
 export default hot(module)(Root)
